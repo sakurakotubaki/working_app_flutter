f188c796bca354e60f75638d498a78c3e3593871
sakurakotubaki <gitonly543@gmail.com>
2026-04-07 18:13:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# working_app_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/jboycode/working_app_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jboycode.working_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
